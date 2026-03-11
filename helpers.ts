a04436a6911f1a99fe5f462c0a73a0e357ea1c65
nurixyildiz <166500762+nurixyildiz@users.noreply.github.com>
2026-03-11 16:07:51 +0300

    Resolve timezone issues in date handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T13:07:36.067Z
-Commit: Introduce real-time notifications
+Timestamp: 2026-03-11T13:07:52.559Z
+Commit: Resolve timezone issues in date handling
