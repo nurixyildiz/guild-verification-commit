b16ded53b773ebf509d4b0eb36e8f453cf7377e5
nurixyildiz <166500762+nurixyildiz@users.noreply.github.com>
2026-03-11 16:19:27 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T13:08:10.637Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-11T13:19:27.879Z
+Commit: Add export functionality for reports
