dca881ed29cef352122a29ad8ba5e4b807191712
nurixyildiz <166500762+nurixyildiz@users.noreply.github.com>
2026-03-11 16:07:36 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T12:59:05.767Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-11T13:07:37.078Z
+Commit: Resolve null pointer exception in user service
