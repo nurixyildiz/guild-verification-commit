18e9038b35c2ac6664c0a9d28aeb1fc1a84f657c
nurixyildiz <166500762+nurixyildiz@users.noreply.github.com>
2026-03-11 16:08:09 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T13:08:06.980Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-11T13:08:10.637Z
+Commit: Fix CSS styling bug on mobile devices
