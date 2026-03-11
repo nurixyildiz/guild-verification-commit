959432128e3aa2aa31c7555179b4e9abe495ab0c
nurixyildiz <166500762+nurixyildiz@users.noreply.github.com>
2026-03-11 16:07:47 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T13:07:38.083Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-11T13:07:48.571Z
+Commit: Fix memory leak in event handlers
