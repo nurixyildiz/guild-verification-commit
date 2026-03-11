ad08eafe5b1468d18b58f21b25cef4a07e08d44c
nurixyildiz <166500762+nurixyildiz@users.noreply.github.com>
2026-03-11 16:19:31 +0300

    Resolve timezone issues in date handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T13:19:25.975Z
-Commit: Add multi-language support
+Timestamp: 2026-03-11T13:19:32.363Z
+Commit: Resolve timezone issues in date handling
