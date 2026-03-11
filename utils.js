b590b8172ffc04b2304ce48d0c26a0cd9f54ca31
nurixyildiz <166500762+nurixyildiz@users.noreply.github.com>
2026-03-11 16:08:06 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-11T13:08:04.826Z
-Commit: Add multi-language support
+Timestamp: 2026-03-11T13:08:06.980Z
+Commit: Patch security vulnerability in authentication
